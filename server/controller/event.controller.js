935ee2fe01a6c60de289005b9ef9bfe3a3136edf
Aditya Gupta <realadityagupta2@gmail.com>
2025-02-25 20:12:14 +0530

    join api created

--- server/controller/event.controller.js
+++ server/controller/event.controller.js
@@ -1,3 +1,5 @@
+import errorHandler from "../middleware/error.js";
+import { io } from "../config/socket.js";
 import Event from "../model/event.model.js";
 export const createEvent = async (req, res, next) => {
   try {
@@ -31,4 +33,38 @@ export const getEvent = async (req, res, next) => {
   console.log(eventId);
   const singleEvent = await Event.findById(eventId);
   res.status(200).json(singleEvent);
+};
+export const joinEvent = async (req, res, next) => {
+  try {
+    const eventId = req.params.eventId;
+    const { userId, fullName } = req.body;
+
+    const existingEvent = await Event.findById(eventId);
+
+    // Check if event exists
+    if (!existingEvent) {
+      return errorHandler(res,404,"Event not found")
+      
+    }
+
+    // Check if user is already registered
+    const userAlreadyRegistered = existingEvent.attendances.some(
+      (attendance) => attendance.id === userId
+    );
+
+    if (userAlreadyRegistered) {
+      return errorHandler(res,400,"User already registered")
+    }
+
+    // Add user to the event
+    existingEvent.attendances.push({ id: userId, fullName });
+    await existingEvent.save();
+
+    // Emit socket event
+    io.to(eventId).emit("attendanceUpdated", existingEvent.attendances);
+
+    res.status(200).json({ message: "User registered successfully" });
+  } catch (error) {
+    next(error);
+  }
 };
--- server/routes/event.route.js
+++ server/routes/event.route.js
@@ -3,10 +3,12 @@ import {
   createEvent,
   getallEvents,
   getEvent,
+  joinEvent,
 } from "../controller/event.controller.js";
 import verifyToken from "../middleware/verify.js";
 const route = express.Router();
 route.post("/create", verifyToken, createEvent);
 route.get("/getallevents", getallEvents);
 route.get("/getevent/:id", getEvent);
+route.post("/:eventId/join",joinEvent)
 export default route;
