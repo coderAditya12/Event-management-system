ef5bfb613125fe2f632df29ae0f109437f8a6822
Aditya Gupta <realadityagupta2@gmail.com>
2025-02-26 10:39:30 +0530

    leave room api created

--- server/controller/event.controller.js
+++ server/controller/event.controller.js
@@ -1,6 +1,7 @@
 import errorHandler from "../middleware/error.js";
 import { io } from "../config/socket.js";
 import Event from "../model/event.model.js";
+import mongoose from "mongoose";
 export const createEvent = async (req, res, next) => {
   try {
     const { title, description, month, time, date, category, location } =
@@ -61,6 +62,40 @@ export const joinEvent = async (req, res, next) => {
     io.to(eventId).emit("attendanceUpdated", existingEvent.attendances);
 
     res.status(200).json({ message: "User registered successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
+export const leaveEvent = async (req, res, next) => {
+  try {
+    const { eventId } = req.params;
+    const { userId } = req.body;
+
+    // 1. Validate Input
+    if (!mongoose.Types.ObjectId.isValid(eventId)) {
+      return errorHandler(res, 400, "Invalid event ID");
+    }
+
+    // 2. Find Event
+    const existingEvent = await Event.findById(eventId);
+    if (!existingEvent) {
+      return errorHandler(res, 404, "Event not found");
+    }
+
+    // 3. Check User Registration
+    const userIndex = existingEvent.attendances.findIndex(
+      (attendance) => attendance.id === userId
+    );
+
+    if (userIndex === -1) {
+      return errorHandler(res, 400, "User not registered");
+    }
+
+    // 4. Remove User
+    existingEvent.attendances.splice(userIndex, 1); 
+    await existingEvent.save();
+    io.to(eventId).emit("attendanceUpdated", existingEvent.attendances);
+    res.status(200).json({ message: "Unregistered successfully" });
   } catch (error) {
     next(error);
   }
--- server/routes/event.route.js
+++ server/routes/event.route.js
@@ -4,11 +4,13 @@ import {
   getallEvents,
   getEvent,
   joinEvent,
+  leaveEvent,
 } from "../controller/event.controller.js";
 import verifyToken from "../middleware/verify.js";
 const route = express.Router();
 route.post("/create", verifyToken, createEvent);
 route.get("/getallevents", getallEvents);
 route.get("/getevent/:id", getEvent);
-route.post("/:eventId/join",joinEvent)
+route.post("/:eventId/join", joinEvent);
+route.delete("/:eventId/leave", leaveEvent);
 export default route;
