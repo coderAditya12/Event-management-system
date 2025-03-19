c2baacabac4f0ec61447b25d6fe4e0ac03e5e6e1
Aditya Gupta <realadityagupta2@gmail.com>
2025-03-19 12:04:48 +0530

    firebase admin setup

--- server/firebase-admin.js
+++ server/firebase-admin.js
@@ -1,5 +1,14 @@
 import admin from "firebase-admin";
-import serviceAccount from "./firebase-admin-key.json" assert { type: "json" };
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const serviceAccount = require("./firebase-admin-key.json");
+
 admin.initializeApp({ credential: admin.credential.cert(serviceAccount) });
 
 export default admin;
+
+// import admin from "firebase-admin";
+// import serviceAccount from "./firebase-admin-key.json" assert { type: "json" };
+// admin.initializeApp({ credential: admin.credential.cert(serviceAccount) });
+
+// export default admin;
