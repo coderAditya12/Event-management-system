d0250d925d39e670bb0b8e52155ea8189e9d80de
Aditya Gupta <realadityagupta2@gmail.com>
2025-03-19 19:00:40 +0530

    signout fixed

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
 
   const handleSignOut = async () => {
     try {
-      const response = await axios.get("http://localhost:3000/api/signout", {
+      const response = await axios.get("/api/signout", {
         withCredentials: true,
       });
 
